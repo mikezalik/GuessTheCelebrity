991e34cd6b3dd95505b442d478b30ccad0df37f3
mikezalik <mike.zalik@gmail.com>
2020-02-07 13:24:01 -0800

    Adds Celeb image URL and bitmap for populating the imageView.

--- app/src/main/java/com/mikezalik/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/mikezalik/guessthecelebrity/MainActivity.java
@@ -7,12 +7,14 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.service.autofill.FieldClassification;
+import android.widget.ImageView;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> celebURLs = new ArrayList<String>();
     ArrayList<String> celebNames = new ArrayList<String>();
+
+    int chosenCeleb = 0;
+
+    ImageView imageView;
 
     public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
 
@@ -33,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 InputStream inputStream = connection.getInputStream();
                 Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
                 return myBitmap;
-                
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -75,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.imageView);
+
         DownloadTask task = new DownloadTask();
         String result = null;
 
@@ -96,6 +104,14 @@ public class MainActivity extends AppCompatActivity {
             while (m.find()) {
                 celebNames.add(m.group(1));
             }
+
+            Random rand = new Random();
+            chosenCeleb = rand.nextInt(celebURLs.size());
+
+            ImageDownloader imageTask = new ImageDownloader();
+            Bitmap celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
 
         } catch (Exception e) {
             e.printStackTrace();
